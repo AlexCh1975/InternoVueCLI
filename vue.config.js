70ccfff60f8ef7b786ef1428da067f24a9c1a8c6
AlexCh1975 <stovertikal@mail.ru>
2023-09-03 17:35:41 +0700

    add interno-project, dz_4 (to seminar 8)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+ 
+      sass: {
+        additionalData: `@import "~@/variables.sass"`
+      },
+
+      scss: {
+        additionalData: `@import "~@/variables.scss";`
+      },
+
+    }
+  }
 })
